47a61b4e4d921bf247118f9d402f909448e9ec59
Fazt <contact@fazweb.com>
2022-10-14 15:10:18 -0600

    Creacion Clase Date, atributos o campos de clase date, constructor de la clase date

---
+++ ConceptsPOO/ConceptsPOO/Date.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConceptsPOO
+{
+    public class Date
+    {
+        private int _year;
+        private int _month;
+        private int _day;
+
+        public Date(int year, int month , int day)
+        {
+            _year = year;
+            _month = month; 
+            _day = day; 
+        }
+        public override string ToString()
+        {
+            return _year + "/"+ _month +"/"+ _day;
+        }
+
+    }
+}
---
+++ ConceptsPOO/ConceptsPOO/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConceptsPOO;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("==============");
+Date date1 = new Date(2022,10,14);
+Console.WriteLine(date1 );
+
+
+
