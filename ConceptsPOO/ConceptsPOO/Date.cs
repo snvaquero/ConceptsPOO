50e1a5540112f5cd6b4d211c79ac3c24853671ae
Fazt <contact@fazweb.com>
2022-10-18 16:00:55 -0600

    Finish date class

--- ConceptsPOO/ConceptsPOO/Date.cs
+++ ConceptsPOO/ConceptsPOO/Date.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,17 +17,40 @@ namespace ConceptsPOO
         {
             _year = year;
             _month = CheckMonth(month); 
-            _day = day; 
+            _day = CheckDay(year,month,day); 
         }
-
         private int CheckMonth(int month)
         {
-            if (month >= 1 && month <=12)
+            if (month >= 1 && month <= 12)
             {
                 return month;
             }
-            throw new NotImplementedException();
+            throw new MonthException("Invalid Month");
         }
+
+        private int CheckDay(int year, int month, int day)
+        {
+            if (month == 2 && day == 29 && IsLeapYear(year))
+            {
+                return day;
+            }
+            int[] daysPerMonth = {0,31,28,31,30,31, 30, 31, 31, 30, 31, 30, 31 };
+            if (day >=1 && day <= daysPerMonth[month])
+            {
+                return day;
+            }
+
+           
+                throw new DayException("Invalid Day");
+            }
+
+        private bool IsLeapYear(int year)
+        {
+            return year % 400 == 0 || year % 4 == 0 && year % 100 != 0;
+                        
+        }
+
+       
 
         public override string ToString()
         {
--- ConceptsPOO/ConceptsPOO/Program.cs
+++ ConceptsPOO/ConceptsPOO/Program.cs
@@ -3,12 +3,19 @@ using ConceptsPOO;
 
 Console.WriteLine("Hello, World!");
 Console.WriteLine("==============");
-Date date1 = new Date(2022,12,7);
-Date date2 = new Date(1974, 9, 23);
-Date date3 = new Date(1985,18,45);
-Console.WriteLine(date1);
-Console.WriteLine(date2);
-Console.WriteLine(date3);
+
+try
+{
+   
+    Console.WriteLine(new Date(2022, 2, 29));
+    Console.WriteLine(new Date(1974, 9, 23));
+    Console.WriteLine(new Date(1985, 12, 45));
+}
+catch (Exception error)
+{
+
+	Console.WriteLine(error.Message); 
+}
 
 
 
