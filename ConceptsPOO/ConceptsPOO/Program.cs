c41141448cd3d4033c30bf1138178a85a696fc90
Fazt <contact@fazweb.com>
2022-10-19 16:23:39 -0600

    Imprimir Empleado1

--- ConceptsPOO/ConceptsPOO/Program.cs
+++ ConceptsPOO/ConceptsPOO/Program.cs
@@ -4,7 +4,17 @@ using ConceptsPOO;
 Console.WriteLine("Hello, World!");
 Console.WriteLine("==============");
 
-Employee employee = new Employee();
+Employee employee1 = new SalaryEmployee()
+{
+    Id = 1010,
+    FirstName = "Sandra",
+    LastName = "Morales",
+    BirthDate = new Date(1990, 5, 3),
+    HiringDate = new Date(2022,1,15),
+    IsActive = true,
+    Salary = 1815453.45M
+};
+Console.WriteLine(employee1);
 
 
 
