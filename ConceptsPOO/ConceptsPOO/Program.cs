322634f0ffc061ef85afdaba1be11bb1c594dc10
Fazt <contact@fazweb.com>
2022-10-19 15:58:17 -0600

    Clase Abstracta Employee y Clase SalaryEmployee que hereda de Clase Employee

--- ConceptsPOO/ConceptsPOO/Program.cs
+++ ConceptsPOO/ConceptsPOO/Program.cs
@@ -4,18 +4,7 @@ using ConceptsPOO;
 Console.WriteLine("Hello, World!");
 Console.WriteLine("==============");
 
-try
-{
-   
-    Console.WriteLine(new Date(2022, 2, 29));
-    Console.WriteLine(new Date(1974, 9, 23));
-    Console.WriteLine(new Date(1985, 12, 45));
-}
-catch (Exception error)
-{
-
-	Console.WriteLine(error.Message); //
-}
+Employee employee = new Employee();
 
 
 
---
+++ ConceptsPOO/ConceptsPOO/SalaryEmployee.cs
@@ -0,0 +1,17 @@
+﻿namespace ConceptsPOO
+{
+    public class SalaryEmployee : Employee
+    {
+        public Decimal Salary { get; set; }
+        public override decimal GetValueToPay()
+        {
+            return Salary;
+        }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()}" +
+                $" \n\tValue to Pay: {GetValueToPay()}";
+        }
+    }
+}
